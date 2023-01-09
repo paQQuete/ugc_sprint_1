662865199221b445c0cc38faada0b0dd0477a678
boris <boris.vladimirov13@gmail.com>
2023-01-10 02:01:17 +0600

    added kafka.yml, initial queries for clickhouse cluster

---
+++ clickhouse/initial.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+from clickhouse_driver import Client
+from dotenv import load_dotenv
+
+
+load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = Client(host=os.getenv('CLICKHOUSE_HOST'))
+
+client.execute('CREATE DATABASE IF NOT EXISTS example ON CLUSTER company_cluster')
+client.execute(
+    'CREATE TABLE example.regular_table ON CLUSTER company_cluster (id Int64, x Int32) Engine=MergeTree() ORDER BY id')
+
+os.system(r"docker exec clickhouse-node1 bash -c 'clickhouse-client --queries-file /etc/clickhouse-server/node1.sql'")
+os.system(r"docker exec clickhouse-node3 bash -c 'clickhouse-client --queries-file /etc/clickhouse-server/node3.sql'")
---
+++ clickhouse/node1/node1.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE shard;
+CREATE DATABASE replica;
+CREATE TABLE shard.test (id Int64, event_time DateTime) Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/test', 'replica_1') PARTITION BY toYYYYMMDD(event_time) ORDER BY id;
+CREATE TABLE replica.test (id Int64, event_time DateTime) Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/test', 'replica_2') PARTITION BY toYYYYMMDD(event_time) ORDER BY id;
+CREATE TABLE default.test (id Int64, event_time DateTime) ENGINE = Distributed('company_cluster', '', test, rand());
