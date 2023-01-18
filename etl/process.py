6744f5e482bbe2f1e66db2e03236e50d4b1f8ea0
boris <boris.vladimirov13@gmail.com>
2023-01-18 15:39:25 +0600

    ETL done

--- etl/process.py
+++ etl/process.py
@@ -10,7 +10,7 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 client = Client(host=os.getenv('CLICKHOUSE_HOST'))
-# kafka = f"{os.getenv('KAFKA_HOST')}:{os.getenv('KAFKA_PORT')}"
+kafka_broker = f"{os.getenv('KAFKABROKER_HOST')}:{os.getenv('KAFKABROKER_PORT')}"
 
 client.execute(f"""
     CREATE TABLE IF NOT EXISTS default.views_queue ON CLUSTER company_cluster
@@ -20,7 +20,7 @@ client.execute(f"""
   movie_timestamp timestamp,
   event_timestamp timestamp
 )
-ENGINE=Kafka('broker:29092', 'views', 'views_group{random.randint(1,256)}', 'JSONEachRow')
+ENGINE=Kafka('{kafka_broker}', 'views', 'views_group{random.randint(1,256)}', 'JSONEachRow')
 settings kafka_thread_per_consumer = 0, kafka_num_consumers = 1;
 """)
 
