b1473febb95165aedecb8121c4c54ee74bc3dedc
boris <boris.vladimirov13@gmail.com>
2023-02-12 23:44:56 +0600

    some changes

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,14 +2,18 @@ import logging
 
 import aiokafka
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import ORJSONResponse
+import sentry_sdk
 
 from api.v1 import views, bookmarks, likes, reviews
 from core import config
 from core.logger import LOGGING
 from db import kafka
 
+
+sentry_sdk.init(
+    dsn="https://d7b229275c864cb9aa5d7a3b7f2ac257@o4504634582302720.ingest.sentry.io/4504662476390400")
 
 app = FastAPI(
     title=config.Settings().PROJECT_NAME,
@@ -43,7 +47,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
-        port=8000,
+        port=9000,
         log_config=LOGGING,
         log_level=logging.DEBUG
     )
